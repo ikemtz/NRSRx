484ee9fe41c1741b93b5caff378c2cb0c34b7f59
Isaac Martinez <ikemtz@outlook.com>
2019-08-28 08:10:10 -0400

    Adding ODataEnvelope.Create method

--- src/IkeMtz.NRSRx.Core.Models/ODataEnvelope.cs
+++ src/IkeMtz.NRSRx.Core.Models/ODataEnvelope.cs
@@ -1,20 +1,30 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace IkeMtz.NRSRx.Core.Models
 {
-    public class ODataEnvelope<Entity> : ODataEnvelope<Entity, Guid>
-         where Entity : class, IIdentifiable, new()
-    {
-    }
+  public class ODataEnvelope<Entity> : ODataEnvelope<Entity, Guid>
+       where Entity : class, IIdentifiable, new()
+  {
+  }
 
-    public class ODataEnvelope<Entity, IdentityType> where Entity : class, IIdentifiable<IdentityType>, new()
-    {
-        [JsonProperty("value")]
-        public IEnumerable<Entity> Value { get; set; }
+  public class ODataEnvelope<Entity, IdentityType> where Entity : class, IIdentifiable<IdentityType>, new()
+  {
+    [JsonProperty("value")]
+    public IEnumerable<Entity> Value { get; set; }
 
-        [JsonProperty("@odata.count")]
-         public int Count { get; set; }
+    [JsonProperty("@odata.count")]
+    public int Count { get; set; }
+
+    public static ODataEnvelope<Entity, IdentityType> Create(IEnumerable<Entity> t)
+    {
+      return new ODataEnvelope<Entity, IdentityType>()
+      {
+        Count = t.Count(),
+        Value = t
+      };
     }
+  }
 }
