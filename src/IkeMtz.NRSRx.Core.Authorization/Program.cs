c5ea3dfb14a791f23ced9372ba5daa1082e52287
Isaac Martinez <ikemtz@outlook.com>
2020-01-03 04:25:37 -0500

    Fixing authorization

---
+++ src/IkeMtz.NRSRx.Core.Authorization/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace IkeMtz.NRSRx.Core.Authorization
+{
+  internal static class Program
+  {
+    internal static void Main()
+    {
+      throw new NotSupportedException();
+    }
+  }
+}
