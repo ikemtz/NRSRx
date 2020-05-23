9ade464f3ef4c58bf9eafe73d7ba068e3d28e308
Isaac Martinez <6444182+ikemtz@users.noreply.github.com>
2020-05-23 10:00:51 -0400

    Adding Code Refactor Tool (#30)
    * Fixing TestContextResponseLogger, handling JsonSerializationException
    
    * Updating nuget packages
    
    * Updating readme badges
    
    * Adding nuget badge
    
    * Addressing code smell
    
    * Refactoring Publisher Unigration Tester
    
    * Refacotring DbContextFactory
    
    * EF library updates
    
    * Fixing warnings
    
    * Cleaning up unit tests
    
    * Adding ping result
    
    * Adding downloads badge
    
    * Updating readme again
    
    * Fixing unit test break
    
    * Refactoring PingResult
    
    * Updating template script
    
    * Adding git tags to build process
    
    * Fixing eol on yaml file
    
    * Persisting credentials to fix git tagging
    
    * Fixing yaml
    
    * Adding tools
    
    * Completing my SQL Model Class Generator
    
    * Updating documentation
    
    * Renaming sql poco class generator
    
    * Adding OData test template
    
    * Completing OData templates
    
    * Additional integration testing cleanup
    
    * Caching produced project files
    
    * Updating extension
    
    * Fixing build break
    
    * Adding Code Refactor Tool
    
    * Updating Code Refactor Tool

---
+++ tools/code-refactor-tool.ps1
@@ -0,0 +1,109 @@
+﻿# Code refactor tool
+# This script will do a find and replace on file names and content
+# Creator Isaac Martinez
+# Example Usage: 
+# .\code-refactor-tool.ps1 -findValue 'SOME_OLD_VALUE' -replacementValue 'SOME_NEW_VALUE'
+
+param (
+  [string]$findValue, # = "Lookup value",
+  [string]$replacementValue  # = "Replacement value"
+ )
+
+Set-Location $PSScriptRoot
+Get-ChildItem -Path $PSScriptRoot -Filter *$findValue* -Recurse | 
+Foreach-Object {
+
+    $replacementValueFileName =  $_.Name.Replace($findValue, $replacementValue);
+    Write-Host "findValue: $_"
+    Write-Host "replacementValue: $replacementValueFileName"
+    
+    Rename-Item $_.FullName $_.FullName.Replace($findValue, $replacementValue);
+}
+
+Get-ChildItem -Path $PSScriptRoot -Filter *.cs -Recurse | 
+Foreach-Object {
+  $fileContents = [System.IO.File]::ReadAllText($_.FullName)
+    if ( $fileContents.Contains($findValue) )
+    {
+     Write-Host "Updating File Content On:" $_.Name
+     $fileContents = $fileContents.Replace($findValue, $replacementValue);
+     [System.IO.File]::WriteAllText($_.FullName, $fileContents)
+    }
+}
+
+Get-ChildItem -Path $PSScriptRoot -Filter *.ps1 -Recurse | 
+Foreach-Object {
+  $fileContents = [System.IO.File]::ReadAllText($_.FullName)
+    if ( $fileContents.Contains($findValue) )
+    {
+     Write-Host "Updating File Content On:" $_.Name
+     $fileContents = $fileContents.Replace($findValue, $replacementValue);
+     [System.IO.File]::WriteAllText($_.FullName, $fileContents)
+    }
+}
+
+Get-ChildItem -Path $PSScriptRoot -Filter *.sql  -Recurse | 
+Foreach-Object {
+  $fileContents = [System.IO.File]::ReadAllText($_.FullName)
+    if ( $fileContents.Contains($findValue) )
+    {
+     Write-Host "Updating File Content On:" $_.Name
+     $fileContents = $fileContents.Replace($findValue, $replacementValue);
+     [System.IO.File]::WriteAllText($_.FullName, $fileContents)
+    }
+}
+
+Get-ChildItem -Path $PSScriptRoot -Filter *.csproj -Recurse | 
+Foreach-Object {
+  $fileContents = [System.IO.File]::ReadAllText($_.FullName)
+    if ( $fileContents.Contains($findValue) )
+    {
+     Write-Host "Updating File Content On:" $_.Name
+     $fileContents = $fileContents.Replace($findValue, $replacementValue);
+     [System.IO.File]::WriteAllText($_.FullName, $fileContents)
+    }
+}
+
+Get-ChildItem -Path $PSScriptRoot -Filter *Dockerfile -Recurse | 
+Foreach-Object {
+  $fileContents = [System.IO.File]::ReadAllText($_.FullName)
+    if ( $fileContents.Contains($findValue) )
+    {
+     Write-Host "Updating File Content On:" $_.Name
+     $fileContents = $fileContents.Replace($findValue, $replacementValue);
+     [System.IO.File]::WriteAllText($_.FullName, $fileContents)
+    }
+}
+
+Get-ChildItem -Path $PSScriptRoot -Filter *.yml -Recurse | 
+Foreach-Object {
+  $fileContents = [System.IO.File]::ReadAllText($_.FullName)
+    if ( $fileContents.Contains($findValue) )
+    {
+     Write-Host "Updating File Content On:" $_.Name
+     $fileContents = $fileContents.Replace($findValue, $replacementValue);
+     [System.IO.File]::WriteAllText($_.FullName, $fileContents)
+    }
+}
+
+Get-ChildItem -Path $PSScriptRoot -Filter *.sln -Recurse | 
+Foreach-Object {
+  $fileContents = [System.IO.File]::ReadAllText($_.FullName)
+    if ( $fileContents.Contains($findValue) )
+    {
+     Write-Host "Updating File Content On:" $_.Name
+     $fileContents = $fileContents.Replace($findValue, $replacementValue);
+     [System.IO.File]::WriteAllText($_.FullName, $fileContents)
+    }
+}
+
+Get-ChildItem -Path $PSScriptRoot -Filter *.ts -Recurse | 
+Foreach-Object {
+  $fileContents = [System.IO.File]::ReadAllText($_.FullName)
+    if ( $fileContents.Contains($findValue) )
+    {
+     Write-Host "Updating File Content On:" $_.Name
+     $fileContents = $fileContents.Replace($findValue, $replacementValue);
+     [System.IO.File]::WriteAllText($_.FullName, $fileContents)
+    }
+}
