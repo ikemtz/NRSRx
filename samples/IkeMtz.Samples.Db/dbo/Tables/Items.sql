db19fb93f318e98e714ec8d1f1099769586228de
Isaac Martinez <6444182+ikemtz@users.noreply.github.com>
2020-06-30 08:15:39 -0600

    Adding integration tests (#48)
    * Small code cleanup
    
    * Resolving issue with access_token in SignalR url
    
    * Updating dependencies
    
    * Removing unnecessary imports
    
    * Increasing code coverage
    
    * Increasing code coverage
    
    * Addressing Max Top Value Issue
    
    * Adding unit tests for $metadata
    
    * Adding integration tests for web api

--- samples/IkeMtz.Samples.Db/dbo/Tables/Items.sql
+++ samples/IkeMtz.Samples.Db/dbo/Tables/Items.sql
@@ -1,7 +1,12 @@
-﻿CREATE TABLE [dbo].[Items]
+CREATE TABLE [dbo].[Items]
 (
   [Id] UNIQUEIDENTIFIER NOT NULL,
   [Value] NVARCHAR (50) NOT NULL,
+  
+  [CreatedBy] NVARCHAR (250) NOT NULL,
+  [UpdatedBy] NVARCHAR (250) NULL,
+  [CreatedOnUtc] DATETIMEOFFSET (7) NOT NULL,
+  [UpdatedOnUtc] DATETIMEOFFSET (7) NULL,
   CONSTRAINT [PK_Items] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
