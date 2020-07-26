987d89b640282425402a73b7989bf030e16d8062
Isaac Martinez <6444182+ikemtz@users.noreply.github.com>
2020-07-26 03:18:30 -0400

    Adding PKCE to swagger pages (#54)
    * Small code cleanup
    
    * Resolving issue with access_token in SignalR url
    
    * Updating dependencies
    
    * Removing unnecessary imports
    
    * Increasing code coverage
    
    * Increasing code coverage
    
    * Addressing Max Top Value Issue
    
    * Adding unit tests for $metadata
    
    * Adding integration tests for web api
    
    * Working on integration testing on build pipeline
    
    * Fixing build
    
    * Fixing build break
    
    * building test projects
    
    * Updating dependencies
    
    * Making changes to the JwtNameClaimMapping
    
    * Applying more updates
    
    * Fixing build process
    
    * Increasing code coverage
    
    * Resolving code coverage issue on test context logger
    
    * Undoing previous change
    
    * Resolving item integration tests
    
    * Adding PKCE to swagger pages
    
    * Increasing code coverage

---
+++ src/IkeMtz.NRSRx.Core.Web/SwaggerAuthorizeOperationFilter.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace IkeMtz.NRSRx.Core.Web
+{
+  public class SwaggerAuthorizeOperationFilter
+         : IOperationFilter
+  {
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+      var authAttributes = context?.MethodInfo.DeclaringType.GetCustomAttributes(true)
+                      .Union(context.MethodInfo.GetCustomAttributes(true))
+                      .OfType<AuthorizeAttribute>();
+
+      if (authAttributes.Any() && operation != null)
+      {
+        operation.Responses.Add(StatusCodes.Status401Unauthorized.ToString(CultureInfo.CurrentCulture), new OpenApiResponse { Description = nameof(HttpStatusCode.Unauthorized) });
+        operation.Responses.Add(StatusCodes.Status403Forbidden.ToString(CultureInfo.CurrentCulture), new OpenApiResponse { Description = nameof(HttpStatusCode.Forbidden) });
+
+        operation.Security = new List<OpenApiSecurityRequirement>();
+
+        var oauth2SecurityScheme = new OpenApiSecurityScheme()
+        {
+          Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "OAuth2" },
+        };
+
+
+        operation.Security.Add(new OpenApiSecurityRequirement()
+        {
+          [oauth2SecurityScheme] = new[] { "OAuth2" }
+        });
+      }
+    }
+  }
+}
