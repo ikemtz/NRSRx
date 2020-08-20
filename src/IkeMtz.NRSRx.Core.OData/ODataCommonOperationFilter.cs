88331d46dc3c4a05ff71fa007640f65bd92326ac
IkeMtz <ikemtz@outlook.com>
2020-08-20 18:20:16 -0400

    Fixing parameter order on odata swagger

--- src/IkeMtz.NRSRx.Core.OData/ODataCommonOperationFilter.cs
+++ src/IkeMtz.NRSRx.Core.OData/ODataCommonOperationFilter.cs
@@ -11,10 +11,10 @@ namespace IkeMtz.NRSRx.Core.OData
     {
       AddParameter(operation, "$filter", "string", "Specifies the logic to pull back a subset of records.");
       AddParameter(operation, "$orderby", "string", "Specifies the values used to sort the collection of entries.");
+      AddParameter(operation, "$apply", "string", "Specifies aggregation behavior for the collection of entries.");
       AddParameter(operation, "$top", "number", "Specifies the subset of entries by count.");
       AddParameter(operation, "$skip", "number", "Specifies the count of entries to skip.");
       AddParameter(operation, "$count", "boolean", "Indicates whether or not to include a total entry count.");
-      AddParameter(operation, "$apply", "string", "Specifies aggregation behavior for the collection of entries.");
     }
 
     public static void AddParameter(OpenApiOperation operation, string name, string type = "string", string description = "")
