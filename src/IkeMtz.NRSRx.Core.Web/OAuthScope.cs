127bd20278abe0d9400fca6ffef2305dae057249
Isaac Martinez <6444182+ikemtz@users.noreply.github.com>
2021-04-01 23:08:48 -0400

    Increasing code coverage surrounding OAuthScopeInfo (#76)
    * Resolving OpenApi Doc spec issue on TestJsonDocAsync
    
    * Upgrading to .net core 5.0
    
    * Resolving vulnerability issues
    
    * Fixing unit test
    
    * Nuget updates
    
    * Fixing Swagger Scopes
    
    * nuget package updates
    
    * Adding OAuthScopeInfo
    
    * Increasing code coverage
    
    * renaming OAuthScope
    
    * Updating identity provider url

--- src/IkeMtz.NRSRx.Core.Web/OAuthScope.cs
+++ src/IkeMtz.NRSRx.Core.Web/OAuthScope.cs
@@ -1,9 +1,8 @@
 namespace IkeMtz.NRSRx.Core.Web
 {
-  public class OAuthScopeInfo
+  public class OAuthScope
   {
-    public OAuthScopeInfo() { }
-    public OAuthScopeInfo(string name, string description)
+    public OAuthScope(string name, string description)
     {
       Name = name;
       Description = description;
