18cf7e1f7a0888292720381e335f5c340ec64e58
Isaac Martinez <6444182+ikemtz@users.noreply.github.com>
2021-07-01 16:36:33 -0400

    Tenancy filter (#90)
    * Adding CoreTenantFilterAttribute
    
    * Nuget updated
    
    * Removing snyk.io from Azure build process
    
    * Adding ITentantable
    
    * Refactoring CoreTenantFilter
    
    * Refactoring CoreTenantFilter

--- tests/IkeMtz.NRSRx.WebApi.Tests/SampleWeb/SampleTenantFilter.cs
+++ tests/IkeMtz.NRSRx.WebApi.Tests/SampleWeb/SampleTenantFilter.cs
@@ -1,13 +1,17 @@
+using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using IkeMtz.NRSRx.Core.Web;
 
 namespace IkeMtz.NRSRx.WebApi.Tests.SampleWeb
 {
   public class SampleTenantFilterAttribute : CoreTenantFilterAttribute
   {
-    public SampleTenantFilterAttribute() : base(
-      (claims) => claims.Where(c => c.Type.StartsWith("t")).Select(c => c.Value))
+    public SampleTenantFilterAttribute() { }
+
+    public override IEnumerable<string> GetUserTenants(IEnumerable<Claim> claims)
     {
+      return claims.Where(c => c.Type.StartsWith("t")).Select(c => c.Value);
     }
   }
 }
