2145337038e4a9b33afad2c44eb917837a246aa1
IkeMtz <ikemtz@outlook.com>
2022-07-01 20:39:05 -0400

    Resolving conflicts with PostAsJsonAsync and PutAsJsonAsync

--- tests/IkeMtz.NRSRx.WebApi.Tests/StudentTests.cs
+++ tests/IkeMtz.NRSRx.WebApi.Tests/StudentTests.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using IkeMtz.NRSRx.Core.Unigration;
+using IkeMtz.NRSRx.Core.Unigration.Http;
 using IkeMtz.NRSRx.OData.Tests;
 using IkeMtz.Samples.Models.V1;
 using IkeMtz.Samples.WebApi;
