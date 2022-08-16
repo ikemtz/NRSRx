b688d74a616c2025a655d17a29b4ce615c39fd16
Isaac Martinez <6444182+ikemtz@users.noreply.github.com>
2022-08-15 20:34:47 -0400

    Performance improvement StringGenerator (#170)
    * Vulnerability update
    
    * safety Updates to simpleMapper
    
    * Bug fix on using JWT client tokens
    
    * Fixing previous fix
    
    * Updating AuditabletestIncerceptor
    
    * Adding error handing for WwwAuthenticate header
    
    * Expanding error handling of missing JwtNameClaimMapping
    
    * Small bug fix on SignalR
    
    * Bug fixes around integration tests
    
    * Adding support for swagger default value on Enums
    
    * Fixing unit tests
    
    * Downgrading StackExchange.Redis
    
    * Expaning Enum helper
    
    * Resolving code smell
    
    * Fixing build pipeline
    
    * Performance improvement StringGenerator
    
    * Updating unit tests
    
    * Updating unit tests
    
    * Fixing unit tests
    
    * Fixing unit tests

---
+++ tests/IkeMtz.NRSRx.Core.Tests/StringGeneratorTests.cs
@@ -0,0 +1,40 @@
+using System.Linq;
+using IkeMtz.NRSRx.Core.Unigration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static IkeMtz.NRSRx.Core.Unigration.TestDataFactory;
+
+namespace IkeMtz.NRSRx.Core.Tests
+{
+  [TestClass]
+  public class StringGeneratorTests : BaseUnigrationTests
+  {
+    [TestMethod]
+    [TestCategory("Unit")]
+    public void GenerateStringWithSpacesTest()
+    {
+      string lastString =null;
+      //Trying to replicate random out of index issue and need enough runs to replicate
+      Enumerable.Range(0, 10000).ToList().ForEach((x) =>
+       {
+         var result = StringGenerator(1000, true); 
+         Assert.AreNotEqual(lastString, result);
+         Assert.IsTrue(990 <= result.Length, "generated string is not long enough, expected: 990, actual: {0}", result.Length);
+         lastString = result;
+       });
+    }
+    [TestMethod]
+    [TestCategory("Unit")]
+    public void GenerateStringsTest()
+    {
+      string lastString = null;
+      //Trying to replicate random out of index issue and need enough runs to replicate
+      Enumerable.Range(0, 10000).ToList().ForEach((x) =>
+      {
+        var result = StringGenerator(1000,false); 
+        Assert.AreNotEqual(lastString, result); 
+        Assert.IsTrue(990 <= result.Length, "generated string is not long enough, expected: 990, actual: {0}", result.Length);
+        lastString = result;
+      });
+    }
+  }
+}
