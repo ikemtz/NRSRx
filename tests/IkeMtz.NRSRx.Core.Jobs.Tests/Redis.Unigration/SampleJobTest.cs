dcf629f9e7b47d153289a0ecebaf5fc600962d88
IkeMtz <ikemtz@outlook.com>
2022-11-14 21:13:04 -0500

    Code cleanup and additional job logging

--- tests/IkeMtz.NRSRx.Core.Jobs.Tests/Redis.Unigration/SampleJobTest.cs
+++ tests/IkeMtz.NRSRx.Core.Jobs.Tests/Redis.Unigration/SampleJobTest.cs
@@ -1,6 +1,8 @@
 using IkeMtz.NRSRx.Core.Unigration;
 using IkeMtz.Samples.Redis.Jobs;
+using IkeMtz.Samples.Tests;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Moq;
 using StackExchange.Redis;
 
@@ -41,13 +43,16 @@ namespace IkeMtz.NRSRx.Core.Jobs.Redis.Tests.Unigration
       };
 
       _ = program.SetupHost(x => x.AddSingleton<SchoolCreatedFunction>());
+      var func = program.JobHost.Services.GetRequiredService<SchoolCreatedFunction>();
+      var logger = program.JobHost.Services.GetRequiredService<ILogger<SchoolCreatedFunction>>();
 
       //act
-      await program.RunAsync();
+      await func.HandleMessageAsync(Factories.SchoolFactory());
 
       //assert
-      program.MockSubscriber.Verify(t => t.GetMessagesAsync(It.Is<int>(x => x == 5)), Times.Once);
-      program.MockSubscriber.Verify(t => t.AcknowledgeMessageAsync(It.IsAny<RedisValue>()), Times.Exactly(2));
+      program.MockSubscriber.Verify(t => t.GetMessagesAsync(It.Is<int>(x => x == 5)), Times.Never);
+      program.MockSubscriber.Verify(t => t.AcknowledgeMessageAsync(It.IsAny<RedisValue>()), Times.Never);
+
     }
   }
 }
