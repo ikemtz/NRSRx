8a819a870824def91ee237b119fe1cb82a1d0147
Isaac Martinez <6444182+ikemtz@users.noreply.github.com>
2022-11-22 10:56:57 -0500

    Fluent change to SetupTestDbContext (#199)
    * Adding job framework
    
    * Increasing code coverage
    
    * Increasing code coverage
    
    * Increasing code coverage
    
    * Fixing factories namespace renaming
    
    * Fixing unit tests
    
    * Increasing code coverage
    
    * Removing ISimplePublisher
    
    * Adding UpdateCount
    
    * Unit test fixes
    
    * Fixing unit tests
    
    * Fixing unit tests
    
    * Increasing code coverage
    
    * Increasing code coverage
    
    * Increasing code coverage
    
    * Bug fix on Continuous message processing, introducing
    
    * Bug fix on NRSRx Job Testing
    
    * Code cleanup and additional job logging
    
    * increasing code coverage
    
    * Applying color to Seri console logs
    
    * Bug fix for subject claim handling
    
    * Fixing Jwt sub Claim type mapping
    
    * Cleaning up namespace
    
    * Removing unit test
    
    * Changing RedisStreamSubscriber to begin at the last point in the stream
    
    * Adding support for Readis StreamInfo retrieval
    
    * Adding Redis GetStreamInfoAsync
    
    * Adding logging to DbContext.SaveChanges/Async
    
    * Fixing build
    
    * Fixing previous mistakes
    
    * Expanding unit test coverage
    
    * Increasing code coverage
    
    * Fluent change to SetupTestDbContext

--- samples/IkeMtz.Samples.OData.Tests/Unigration/UnigrationODataTestStartup.cs
+++ samples/IkeMtz.Samples.OData.Tests/Unigration/UnigrationODataTestStartup.cs
@@ -12,9 +12,7 @@ namespace IkeMtz.Samples.OData.Tests.Unigration
     {
     }
 
-    public override void SetupDatabase(IServiceCollection services, string dbConnectionString)
-    {
+    public override void SetupDatabase(IServiceCollection services, string dbConnectionString) =>
       services.SetupTestDbContext<DatabaseContext>();
-    }
   }
 }
