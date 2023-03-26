cc9d78e70e905acbc5b7acbd2d1250ba95205b6c
Isaac Martinez (@ikemtz) <ikemtz@outlook.com>
2023-03-26 04:11:05 -0400

    Sample message fuction test fix

--- src/Events/IkeMtz.NRSRx.Jobs.Redis/SplitMessageFunction.cs
+++ src/Events/IkeMtz.NRSRx.Jobs.Redis/SplitMessageFunction.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using IkeMtz.NRSRx.Core.Jobs;
 using IkeMtz.NRSRx.Core.Models;
 using IkeMtz.NRSRx.Events;
@@ -18,35 +19,43 @@ namespace IkeMtz.NRSRx.Jobs.Redis
       : base(logger, subscriber)
     { }
 
+    [ExcludeFromCodeCoverage]
     public override async Task ProcessStreamsAsync(string messageType, Func<int?,
       Task<IEnumerable<(RedisValue Id, SplitMessage<TEntity> Entity)>>> getMessageFunction)
     {
-      Logger.LogInformation("Pulling {MessageBufferCount} {messageType} messages from queue.", MessageBufferCount, messageType);
-      var messages = await getMessageFunction(MessageBufferCount);
-      var processedMessageCount = 0;
-      var messageCount = 0;
-      while ((messageCount = messages.Count()) > 0)
+      IEnumerable<(RedisValue Id, SplitMessage<TEntity> Entity)> messages;
+      do
       {
-        Logger.LogInformation("Received {messageCount} {messageType} messages from queue.", messageCount, messageType);
-        foreach (var (redisId, entity) in messages)
+        messages = await getMessageFunction(MessageBufferCount);
+        await ProcessStreamSplitBatchAsync(messageType, messages);
+      }
+      while (messages.Any());
+    }
+
+    public virtual async Task ProcessStreamSplitBatchAsync(string messageType, IEnumerable<(RedisValue Id, SplitMessage<TEntity> Entity)> messages)
+    {
+      Logger.LogInformation("Pulling {MessageBufferCount} {messageType} messages from queue.", MessageBufferCount, messageType);
+      Logger.LogInformation("Received {messageCount} {messageType} messages from queue.", messages.Count(), messageType);
+      int processedMessageCount = 0;
+      foreach (var (redisId, entity) in messages)
+      {
+        var id = entity.Id;
+        try
         {
-          var id = entity.Id;
-          try
-          {
-            Logger.LogInformation("Handling {messageType} message with entity id: {id} ", messageType, id);
-            await HandleMessageAsync(entity);
-            Logger.LogInformation("Handled {messageType} message with entity id: {id} ", messageType, id);
-            await Subscriber.AcknowledgeMessageAsync(redisId);
-            await NotifySplitProgress(entity, true);
-            processedMessageCount++;
-          }
-          catch (Exception x)
-          {
-            Logger.LogError(x, "An error while handling {messageType} message with entity id: {id} ", messageType, id);
-            await NotifySplitProgress(entity, false);
-          }
+          Logger.LogInformation("Handling {messageType} message with entity id: {id} ", messageType, id);
+          await HandleMessageAsync(entity);
+          Logger.LogInformation("Handled {messageType} message with entity id: {id} ", messageType, id);
+          await Subscriber.AcknowledgeMessageAsync(redisId);
+          await NotifySplitProgress(entity, true);
+          processedMessageCount++;
+        }
+        catch (Exception x)
+        {
+          Logger.LogError(x, "An error while handling {messageType} message with entity id: {id} ", messageType, id);
+          await NotifySplitProgress(entity, false);
         }
       }
+
       Logger.LogInformation("Processed {processedMessageCount} {messageType} messages from queue.", processedMessageCount, messageType);
     }
 
