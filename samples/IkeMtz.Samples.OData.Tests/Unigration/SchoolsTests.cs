1a299762ea464b8314a75fb2938383566b274cf4
Isaac Martinez <6444182+ikemtz@users.noreply.github.com>
2023-11-23 16:27:27 -0500

    Security updates (#239)

--- samples/IkeMtz.Samples.OData.Tests/Unigration/SchoolsTests.cs
+++ samples/IkeMtz.Samples.OData.Tests/Unigration/SchoolsTests.cs
@@ -43,6 +43,33 @@ namespace IkeMtz.Samples.OData.Tests.Unigration
 
     [TestMethod]
     [TestCategory("Unigration")]
+    public async Task GetSchoolsWithCourseCountTest()
+    {
+      var dbSchool = Factories.SchoolFactory();
+      dbSchool.SchoolCourses.Add(Factories.SchoolCourseFactory(dbSchool, Factories.CourseFactory()));
+      using var srv = new TestServer(TestHostBuilder<Startup, UnigrationODataTestStartup>()
+          .ConfigureTestServices(x =>
+          {
+            ExecuteOnContext<DatabaseContext>(x, db =>
+            {
+              _ = db.Schools.Add(dbSchool);
+            });
+          })
+       );
+      var client = srv.CreateClient(TestContext);
+      GenerateAuthHeader(client, GenerateTestToken());
+
+      var resp = await client.GetStringAsync($"odata/v1/{nameof(School)}s?$count=true&$expand={nameof(SchoolCourse)}s($count=true;$top=0)");
+
+      //Validate OData Result
+      TestContext.WriteLine($"Server Reponse: {resp}");
+      var envelope = JsonConvert.DeserializeObject<ODataEnvelope<School>>(resp);
+      StringAssert.Contains(resp, "schoolCourses@odata.count\":1");
+      Assert.AreEqual(dbSchool.Name, envelope?.Value.First().Name);
+    }
+
+    [TestMethod]
+    [TestCategory("Unigration")]
     public async Task DeleteSchoolTest()
     {
       using var srv = new TestServer(TestHostBuilder<Startup, UnigrationODataTestStartup>());
