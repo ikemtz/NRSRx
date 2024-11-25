c79330261df240eb29aabf6605d2038399f6107f
Isaac Martinez (@ikemtz) <ikemtz@outlook.com>
2024-11-25 15:27:44 -0500

    Fixing unit tests

--- tests/IkeMtz.NRSRx.Core.Tests/TestDbContext.cs
+++ tests/IkeMtz.NRSRx.Core.Tests/TestDbContext.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace IkeMtz.NRSRx.Core.Tests
 {
   public class TestDbContext : DbContext
   {
+    public TestDbContext(DbContextOptions<TestDbContext> options) : base(options)
+    {
+    }
     public DbSet<MyIntModel> MyModel { get; set; }
     public DbSet<CollectionGuidModel> CollectionGuidModels { get; set; }
     public DbSet<CollectionIntModel> CollectionIntModels { get; set; }
